c2b745e14551fa1db9511e5dbbd859861e71e716
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-06 02:43:12 +0500

    add new dict comp

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -1,1 +1,10 @@
-print("hello world")
+sqaure = {f"The sqaure of {num} is": num**2 for num in range(1, 11)}
+print(sqaure)
+
+for a, b in sqaure.items():
+    print(f"{a} {b}")
+
+name = "izhar"
+
+string = {char: name.count(char) for char in name}
+print(string)
