e9e7f01c80ee64c5a107bd90aea6176900de1762
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 08:53:09 +0500

    add list comprehension in list

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,13 +1,25 @@
-# Normal function with list
+# Normal reverse function with list
 
-def str_reverse(list1,list2):
+
+def str_reverse(list1, list2):
     name = []
     name2 = []
     for i in list1:
         name.append(i[::-1])
     for j in list2:
         name2.append(j[::-1])
-    return name , name2
+    return name, name2
 
-print(str_reverse(["izhar","sheryar"],["ahmed","umair"]))
 
+print(str_reverse(["izhar", "sheryar"], ["ahmed", "umair"]))
+
+# Reverse Function with List comprehension
+
+
+def str_reverse1(list1, list2):
+    name1 = [name[::-1] for name in list1]
+    name2 = [name[::-1] for name in list2]
+    return name1, name2
+
+
+print(str_reverse(["izhar", "shery"], ["ahmed", "umair"]))
