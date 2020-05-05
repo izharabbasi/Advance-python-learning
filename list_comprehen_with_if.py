eb8df057afa323ac23a6e2f338cfd51a2485e6d0
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 09:19:28 +0500

    if with normal list

---
+++ list_comprehen_with_if.py
@@ -0,0 +1,13 @@
+# Normal If statement with List
+
+number = list(range(1, 11))
+even_number = []
+for num in number:
+    if num % 2 == 0:
+        even_number.append(num)
+        
+print(even_number)
+
+
+
+
