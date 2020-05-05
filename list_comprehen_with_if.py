09a0beb1e14f1e99f0241d8666c9fb4645527a47
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 09:22:45 +0500

    if statment with list comprehension

--- list_comprehen_with_if.py
+++ list_comprehen_with_if.py
@@ -5,9 +5,12 @@ even_number = []
 for num in number:
     if num % 2 == 0:
         even_number.append(num)
-        
+
 print(even_number)
 
 
+# if statement with list comprehension
 
+even_num = [num for num in range(1, 11) if num % 2 == 0]
+print(even_num)
 
