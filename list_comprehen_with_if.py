0fbf6bd1d0e0dbcc784ff48c47076f5387e7ecba
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 09:25:27 +0500

    odd numbers finder

--- list_comprehen_with_if.py
+++ list_comprehen_with_if.py
@@ -10,7 +10,9 @@ print(even_number)
 
 
 # if statement with list comprehension
-
-even_num = [num for num in range(1, 11) if num % 2 == 0]
+digit = [1,2,3,4,5,6,7,8,9,10]
+even_num = [num for num in digit if num % 2 == 0]
 print(even_num)
 
+odd_num = [num for num in range(1,11) if num %2==1]
+print(odd_num)
