dbd34dc352d49f4856a76891f7cbcbd53d2e1451
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-05 08:43:29 +0500

    normal function with list

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,2 +1,13 @@
-print("hello world")
+# Normal function with list
+
+def str_reverse(list1,list2):
+    name = []
+    name2 = []
+    for i in list1:
+        name.append(i[::-1])
+    for j in list2:
+        name2.append(j[::-1])
+    return name , name2
+
+print(str_reverse(["izhar","sheryar"],["ahmed","umair"]))
 
