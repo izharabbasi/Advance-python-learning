eed210aed380c7e3c0e30a042ac55b5447381d5e
Izhar Abaasi <izharabbasi103@gmail.com>
2020-05-06 02:48:07 +0500

    add odd even

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -1,10 +1,21 @@
-sqaure = {f"The sqaure of {num} is": num**2 for num in range(1, 11)}
-print(sqaure)
+# sqaure = {f"The sqaure of {num} is": num**2 for num in range(1, 11)}
+# print(sqaure)
 
-for a, b in sqaure.items():
-    print(f"{a} {b}")
+# for a, b in sqaure.items():
+#     print(f"{a} {b}")
 
-name = "izhar"
+# name = "izhar"
 
-string = {char: name.count(char) for char in name}
-print(string)
+# string = {char: name.count(char) for char in name}
+# print(string)
+
+odd_even = {num:('even' if num %2==0 else 'odd') for num in range(1,11)}
+print(odd_even)
+
+
+
+
+
+
+
+
